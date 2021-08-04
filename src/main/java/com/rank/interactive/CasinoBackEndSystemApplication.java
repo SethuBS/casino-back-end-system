e9e89a078c5566d334746cf1ff77b28bd910dfb2
Sethu <sethu@terrafirma-solutions.com>
2021-08-04 19:55:20 +0200

    project structure and application.properties

---
+++ src/main/java/com/rank/interactive/CasinoBackEndSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rank.interactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoBackEndSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasinoBackEndSystemApplication.class, args);
+	}
+
+}
